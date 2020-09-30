29239641cd804e411b72512c837a3dfa6334418a
koyo-miyamura <nexus9071@gmail.com>
2020-09-30 21:34:49 +0900

    first commit

---
+++ lib/pagex.ex
@@ -0,0 +1,18 @@
+defmodule Pagex do
+  @moduledoc """
+  Documentation for `Pagex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pagex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pagex_test.exs
@@ -0,0 +1,8 @@
+defmodule PagexTest do
+  use ExUnit.Case
+  doctest Pagex
+
+  test "greets the world" do
+    assert Pagex.hello() == :world
+  end
+end
