7f050f8319c07e46d1034bd592f90e285667f3ed
koyo-miyamura <nexus9071@gmail.com>
2020-09-30 23:48:07 +0900

    Simple implement

--- lib/pagex.ex
+++ lib/pagex.ex
@@ -3,16 +3,55 @@ defmodule Pagex do
   Documentation for `Pagex`.
   """
 
+  defmodule Page do
+    defstruct [:list, :current_list, :per_page, :current_page]
+  end
+
+  def page(list, current_list, per_page, current_page) do
+    %Page{
+      list: list,
+      current_list: current_list,
+      per_page: per_page,
+      current_page: current_page
+    }
+  end
+
   @doc """
-  Hello world.
+  Calc next page.
 
   ## Examples
 
-      iex> Pagex.hello()
-      :world
+      iex> %Pagex.Page{list: [1, 2, 3, 4, 5, 6, 7, 8], current_list: [], per_page: 3, current_page: 0} |> Pagex.next
+      %Pagex.Page{
+        current_list: [1, 2, 3],
+        current_page: 1,
+        list: [1, 2, 3, 4, 5, 6, 7, 8],
+        per_page: 3
+      }
+
+      iex> %Pagex.Page{list: [1, 2, 3, 4, 5, 6, 7, 8], current_list: [], per_page: 3, current_page: 0} |> Pagex.next |> Pagex.next
+      %Pagex.Page{
+        current_list: [4, 5, 6],
+        current_page: 2,
+        list: [1, 2, 3, 4, 5, 6, 7, 8],
+        per_page: 3
+      }
+
+      iex> %Pagex.Page{list: [1, 2, 3, 4, 5, 6, 7, 8], per_page: 3, current_page: 2} |> Pagex.next
+      %Pagex.Page{
+        current_list: [7, 8],
+        current_page: 3,
+        list: [1, 2, 3, 4, 5, 6, 7, 8],
+        per_page: 3
+      }
 
   """
-  def hello do
-    :world
+  def next(%Page{list: list, current_list: _, per_page: per_page, current_page: current_page}) do
+    %Page{
+      list: list,
+      current_list: list |> Enum.slice(current_page * per_page, per_page),
+      per_page: per_page,
+      current_page: current_page + 1
+    }
   end
 end
--- test/pagex_test.exs
+++ test/pagex_test.exs
@@ -1,8 +1,4 @@
 defmodule PagexTest do
   use ExUnit.Case
   doctest Pagex
-
-  test "greets the world" do
-    assert Pagex.hello() == :world
-  end
 end
